150c6b9bd13d46d235e4881cb8bfc6f4094090a2
Trung Đào <daoquangtrung1998@outlook.com.vn>
2018-06-04 09:41:42 +0700

    First Commit

--- Chapter19-IntroductionToWinform/Form1.Designer.cs
+++ Chapter19-IntroductionToWinform/Form1.Designer.cs
@@ -28,12 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblName = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(40, 68);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(53, 20);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(107, 65);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(146, 22);
+            this.tbName.TabIndex = 1;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(143, 119);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 29);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.HotPink;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Button btnSubmit;
     }
 }
 
